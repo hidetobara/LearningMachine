f0d7727464931f853fbe82fba505a67ee3b5606c
hidetobara <hidetobara@gmail.com>
2017-01-21 00:55:46 +0900

    パラメータを調整中。

--- IconLibrary/Nodes/LearningNodeImageFilter.cs
+++ IconLibrary/Nodes/LearningNodeImageFilter.cs
@@ -15,23 +15,23 @@ namespace IconLibrary
 		public LearningNodeImageFilter()
 		{
 			_Nodes = new List<LearningNode>();
-			_Nodes.Add(new LearningNodeDuplicator(0, -1));	// コピー
-			_Nodes.Add(new LearningIPCA_Slicing(3, 16));    // 64-3
-			_Nodes.Add(new LearningPool(2));                // 64-16
-			_Nodes.Add(new LearningNormalize());            // 32-16
+			_Nodes.Add(new LearningNodeDuplicator(0, -1));		// コピー
+			_Nodes.Add(new LearningIPCA_Slicing(3, 16, 4));		// 64-3
+			_Nodes.Add(new LearningPool(2));					// 64-16
+			_Nodes.Add(new LearningNormalize());				// 32-16
 
-			_Nodes.Add(new LearningNodeDuplicator(0, -2));  // コピー
-			_Nodes.Add(new LearningIPCA_Slicing(16, 32));    // 32-16
-			_Nodes.Add(new LearningPool(2));                // 32-32
-			_Nodes.Add(new LearningNormalize());            // 16-32
+			_Nodes.Add(new LearningNodeDuplicator(0, -2));		// コピー
+			_Nodes.Add(new LearningIPCA_Slicing(16, 32, 4));	// 32-16
+			_Nodes.Add(new LearningPool(2));					// 32-32
+			_Nodes.Add(new LearningNormalize());				// 16-32
 
 			_Nodes.Add(new LearningNodeScaler(2));
-			var dnnc1 = new LearningDNNConvolution(32, 32, 16, 64);
+			var dnnc1 = new LearningDNNConvolution(32, 32, 16, 128);
 			dnnc1.OutputReference = -2;
 			_Nodes.Add(dnnc1);
 
 			_Nodes.Add(new LearningNodeScaler(2));
-			var dnnc2 = new LearningDNNConvolution(64, 16, 3, 64);
+			var dnnc2 = new LearningDNNConvolution(64, 16, 3, 128);
 			dnnc2.OutputReference = -1;
 			_Nodes.Add(dnnc2);
 		}
